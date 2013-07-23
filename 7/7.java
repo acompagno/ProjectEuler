e875587ce9b35345353a0957596e9232e40e7e8c
acompagno <acompag@gmail.com>
2013-07-23 03:31:24 -0400

    #7 Done in Java

---
+++ 7/7.java
@@ -0,0 +1,46 @@
+class main  {
+    public static boolean isprime(int n)
+    {
+        int i;
+        if (n < 2)
+        {
+            return false ;
+        }
+        else if (n == 2)
+        {
+            return true ;
+        }
+        else if (n % 2 == 0)
+        {
+            return false ;
+        }
+        else
+        {
+            for(i = 3 ; i < (int)(Math.sqrt(n))+2 ; i=i+2)
+            {
+                if (n%i == 0)
+                {
+                    return false;
+                }
+            } 
+            return true;
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        int i = 1 ,count = 0;
+        while (true)
+        {
+            if (isprime(i))
+            {
+                count++;
+                if(count == 10001)
+                {
+                    System.out.println("Answer: "+i);
+                }
+            }
+            i++;
+        }
+    }
+}
